dc43f04be2e682329fe28cb69a0f97bc8a470455
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-12-01 17:20:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName= "bob"
+
+function upperCaseCustomerName(){
+   return (customerName.toUpperCase());
+}
+function setBestCustomer(){
+    var bestCustomer= "not bob";
+    return (bestCustomer);
+}
+
+
+function overwriteBestCustomer(){
+    let bestCustomer="maybe bob";
+    return (bestCustomer);
+}
+
+
+const leastFavoriteCustomer="cindy"
+
+function changeLeastFavoriteCustomer () {
+    const leastFavoriteCustomer="cindy"
+    const leastFavoriteCustomer ="mary";
+   return (leastFavoriteCustomer); 
+}
